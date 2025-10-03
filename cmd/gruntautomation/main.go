454fa35ab954d595884c3eb4e4484c5c0c79f185
Nuru <ceruks@eteb.me>
2025-10-03 19:24:55 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/gruntautomation/main.go
@@ -0,0 +1,23 @@
+// cmd/gruntautomation/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "gruntautomation/internal/gruntautomation"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := gruntautomation.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
